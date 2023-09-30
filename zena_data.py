d76a21d00702ba6b32463d172434a6e3c0b1e6bf
raspberry <jamangi.jmg@gmail.com>
2023-09-30 11:00:54 -0500

    Daily workout update

--- zena_data.py
+++ zena_data.py
@@ -4,5 +4,6 @@ ZENA_DATA = [
     {"date": "2023-09-23", "distance_miles": 2.91, "time_minutes": 65, "name": "Zena"},
     {"date": "2023-09-25", "distance_miles": 3.25, "time_minutes": 65, "name": "Zena"},
     {"date": "2023-09-27", "distance_miles": 3.74, "time_minutes": 65, "name": "Zena"},
-    {"date": "2023-09-29", "distance_miles": 3.80, "time_minutes": 65, "name": "Zena"}
+    {"date": "2023-09-29", "distance_miles": 3.80, "time_minutes": 65, "name": "Zena"},
+    {"date": "2023-09-30", "distance_miles": 3.94, "time_minutes": 65, "name": "Zena"}
 ]
