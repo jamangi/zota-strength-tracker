5d7713c9bc24e8132ebef02914e1cceddc33ac0b
raspberry <jamangi.jmg@gmail.com>
2023-10-02 07:10:13 -0500

    scheduled update

--- sota_data.py
+++ sota_data.py
@@ -6,7 +6,8 @@ SOTA_DATA = [
     {"date": "2023-09-23", "distance_miles": 2.84, "time_minutes": 30, "name": "Sota"},
     {"date": "2023-09-25", "distance_miles": 2.32, "time_minutes": 30, "name": "Sota"},
     {"date": "2023-09-27", "distance_miles": 2.67, "time_minutes": 30, "name": "Sota"},
-    {"date": "2023-09-29", "distance_miles": 2.79, "time_minutes": 83, "name": "Sota"}
+    {"date": "2023-09-29", "distance_miles": 2.79, "time_minutes": 83, "name": "Sota"},
+    {"date": "2023-10-01", "distance_miles": 1.62, "time_minutes": 56, "name": "Sota"}
 ]
 
 SOTA_CONSISTENCY_TUPLES = [(datum['time_minutes'], util.to_date(datum['date'])) for datum in SOTA_DATA]
--- zena_data.py
+++ zena_data.py
@@ -7,7 +7,8 @@ ZENA_DATA = [
     {"date": "2023-09-25", "distance_miles": 3.25, "time_minutes": 65, "name": "Zena"},
     {"date": "2023-09-27", "distance_miles": 3.74, "time_minutes": 65, "name": "Zena"},
     {"date": "2023-09-29", "distance_miles": 3.80, "time_minutes": 65, "name": "Zena"},
-    {"date": "2023-09-30", "distance_miles": 3.94, "time_minutes": 65, "name": "Zena"}
+    {"date": "2023-09-30", "distance_miles": 3.94, "time_minutes": 65, "name": "Zena"},
+    {"date": "2023-10-02", "distance_miles": 3.44, "time_minutes": 65, "name": "Zena"}
 ]
 
 ZENA_CONSISTENCY_TUPLES = [(datum['time_minutes'], util.to_date(datum['date'])) for datum in ZENA_DATA]
