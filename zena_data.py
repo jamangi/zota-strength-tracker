0319fb53e52aed429027bf3f362aeb78e538679f
raspberry <jamangi.jmg@gmail.com>
2023-10-04 10:18:19 -0500

    Update repo image

--- zena_data.py
+++ zena_data.py
@@ -8,7 +8,8 @@ ZENA_DATA = [
     {"date": "2023-09-27", "distance_miles": 3.74, "time_minutes": 65, "name": "Zena"},
     {"date": "2023-09-29", "distance_miles": 3.80, "time_minutes": 65, "name": "Zena"},
     {"date": "2023-09-30", "distance_miles": 3.94, "time_minutes": 65, "name": "Zena"},
-    {"date": "2023-10-02", "distance_miles": 3.44, "time_minutes": 65, "name": "Zena"}
+    {"date": "2023-10-02", "distance_miles": 3.44, "time_minutes": 65, "name": "Zena"},
+    {"date": "2023-10-04", "distance_miles": 3.22, "time_minutes": 65, "name": "Zena"}
 ]
 
 ZENA_CONSISTENCY_TUPLES = [(datum['time_minutes'], util.to_date(datum['date'])) for datum in ZENA_DATA]
